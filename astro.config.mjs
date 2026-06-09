a100be9d31bec158560df08895a6f8bc49732983
suqingji <495695819@qq.com>
2026-06-09 19:12:09 +0800

    Fix err

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,33 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
 // import tailwind from '@astrojs/tailwind';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://suqingji.github.io', 
   base: '/AI-Infra-Notebook', 
+
+  // 👇 2. 告诉 Astro 的 Markdown 解析器使用这两个引擎
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+
   integrations: [
       starlight({
           title: '主页',
+
+          head: [
+            {
+              tag: 'link',
+              attrs: {
+                rel: 'stylesheet',
+                href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+              },
+            },
+          ],
+          
           social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/suqingji/AI-Infra-Notebook' }],
           sidebar: [
               {
